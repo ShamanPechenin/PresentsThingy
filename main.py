c71539b9b36eba9930ea15dadd4a7bfc6d814cb7
ShamanPechenin <60006461+shamanpechenin@users.noreply.github.com>
2024-12-12 18:10:16 +0200

    Added initial files

---
+++ main.py
@@ -0,0 +1,17 @@
+all_davans = [{
+    "name": "100 euro",
+    "price": 100,
+    "picked": False
+}, {
+
+    "name": "puteklis",
+    "price": 170,
+    "picked": False
+
+}, {
+
+    "name": "puteklis",
+    "price": 170,
+    "picked": False
+
+}]
